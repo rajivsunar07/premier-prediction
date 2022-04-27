282fcdb66604051bab9dcbba2768b7e4a44c7aad
rajivsunar <rajivsunar07@gmail.com>
2022-04-27 17:46:46 +0545

    fixtures changed

--- sports/settings.py
+++ sports/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://premier-prediction.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
